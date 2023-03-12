37f513a14a96e890ee7139afd1e8b34a2b1d8bb2
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-12 09:38:45 +0700

    release

--- go.mod
+++ go.mod
@@ -3,15 +3,18 @@ module github.com/aiteung/simpati
 go 1.19
 
 require (
+	github.com/aiteung/musik v0.0.5
 	github.com/whatsauth/wasocket v0.0.4
 	github.com/whatsauth/watoken v0.1.0
 )
 
 require (
+	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/fasthttp/websocket v1.5.0 // indirect
 	github.com/gofiber/fiber/v2 v2.41.0 // indirect
 	github.com/gofiber/websocket/v2 v2.1.3 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -21,12 +24,15 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.mau.fi/libsignal v0.1.0 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20230306190159-5caded34a872 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
 
 require (
 	aidanwoods.dev/go-paseto v1.2.0 // indirect
 	github.com/aiteung/atmodel v0.0.1
 	github.com/whatsauth/whatsauth v0.1.1
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 )
--- simpati.go
+++ simpati.go
@@ -1,6 +1,69 @@
 package simpati
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/aiteung/musik"
+)
+
+func GetUsernamefromPhonenumber(phone_number string, db *sql.DB) (username string) {
+	username = GetUsernamefromPhonenumberInTable(phone_number, "simak_mst_mahasiswa", db)
+	fmt.Println(username)
+	if username == "" {
+		username = GetUsernamefromPhonenumberInTable(phone_number, "simak_mst_dosen", db)
+
+	}
+	return username
+}
+
+func GetUsernamefromPhonenumberInTable(phone_number string, tabel string, db *sql.DB) (username string) {
+	err := db.QueryRow("select Login from "+tabel+" where Handphone = ?", phone_number).Scan(&username)
+	if err != nil {
+		fmt.Printf("GetUsernamefromPhonenumberInTable %v: %v\n", tabel, err)
+	}
+	return username
+}
+
+func GetHashPasswordfromUsername(username string, db *sql.DB) (hashpassword string) {
+	err := db.QueryRow("select user_password from simak_besan_users where user_name = ?", username).Scan(&hashpassword)
+	if err != nil {
+		fmt.Printf("GetHashPasswordfromUsername: %v\n", err)
+	}
+	return hashpassword
+}
+
+func UpdatePasswordfromUsername(username string, db *sql.DB) (newPassword string) {
+	newPassword = musik.RandomString(10)
+	var temp interface{}
+	var err error
+
+	err = db.QueryRow("update simak_mst_mahasiswa set Password = MD5(MD5(?)) where Login = ?", newPassword, username).Scan(&temp)
+	if err != nil {
+		fmt.Printf("UpdatePasswordfromUsername: %v\n", err)
+	}
+	err = db.QueryRow("update simak_mst_dosen set Password = MD5(MD5(?)) where Login = ?", newPassword, username).Scan(&temp)
+	if err != nil {
+		fmt.Printf("UpdatePasswordfromUsername: %v\n", err)
+	}
+	err = db.QueryRow("update simak_besan_users  set user_password = MD5(MD5(?)) where user_name = ?", newPassword, username).Scan(&temp)
+	if err != nil {
+		fmt.Printf("UpdatePasswordfromUsername: %v\n", err)
+	}
+	err = db.QueryRow("update besan_users  set user_password = MD5(MD5(?)) where user_name = ?", newPassword, username).Scan(&temp)
+	if err != nil {
+		fmt.Printf("UpdatePasswordfromUsername: %v\n", err)
+	}
+	return newPassword
+}
+
+func GetUserIdfromUsername(username string, db *sql.DB) (userid string) {
+	err := db.QueryRow("select user_id from simak_besan_users where user_name = ?", username).Scan(&userid)
+	if err != nil {
+		fmt.Printf("GetHashPasswordfromUsername: %v\n", err)
+	}
+	return userid
+}
 
 func GetJadwalKuliah(kodedosen string, db *sql.DB) (jadwal []JadwalKuliah) {
 	rows, _ := db.Query("select JadwalID, Nama, NamaKelas, HariID, JamMulai, JamSelesai, RuangID, Kehadiran from simak_trn_jadwal where DosenID = ? and TahunID = ?", kodedosen, getTahunID(db))
@@ -16,4 +79,5 @@ func GetJadwalKuliah(kodedosen string, db *sql.DB) (jadwal []JadwalKuliah) {
 func getTahunID(db *sql.DB) (TahunID TahunID) {
 	db.QueryRow("SELECT TahunID FROM simak_mst_tahun where NA = 'N' group by TahunID order by TahunID DESC limit 1").Scan(&TahunID)
 	return TahunID
+
 }
